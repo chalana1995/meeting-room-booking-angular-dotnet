23ced22fef55cde381d8c4d4af34bc91758e450c
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-08-02 20:37:49 +0530

    add routing

--- mySpace/src/app/app-routing.module.ts
+++ mySpace/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PackagesComponent } from './pages/packages/packages.component';
+import { ClientComponent } from './pages/client/client.component';
+import { PackageActivationComponent } from './pages/package-activation/package-activation.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'package',
+    component: PackagesComponent,
+  },
+  {
+    path: 'client',
+    component: ClientComponent,
+  },
+  {
+    path: 'package-activation',
+    component: PackageActivationComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
